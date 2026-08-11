1c14dec3be98ed5b0c07ad6f0269d73acf035aeb
Emma Gaxiola <emma.gaxiol@gmail.com>
2026-08-10 20:31:15 -0600

    add custom tooltips

--- src/main/java/net/emmanem/tutorialmod/item/custom/ChiselItem.java
+++ src/main/java/net/emmanem/tutorialmod/item/custom/ChiselItem.java
@@ -3,18 +3,24 @@ package net.emmanem.tutorialmod.item.custom;
 import net.emmanem.tutorialmod.block.ModBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.world.World;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class ChiselItem extends Item {
@@ -66,5 +72,15 @@ public class ChiselItem extends Item {
         }
 
         return ActionResult.PASS;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        if(Screen.hasShiftDown()) {
+            tooltip.add(Text.translatable("tooltip.tutorialmod.chisel.shift_down"));
+        } else {
+            tooltip.add(Text.translatable("tooltip.tutorialmod.chisel"));
+        }
+        super.appendTooltip(stack, context, tooltip, type);
     }
 }
