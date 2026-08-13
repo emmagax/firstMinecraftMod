d4bfd0b8f3f9659fc1ce7e0c1b1c7d97dd4c04c6
Emma Gaxiola <emma.gaxiol@gmail.com>
2026-08-12 18:53:47 -0600

    added custom music disc

--- src/main/java/net/emmanem/tutorialmod/item/ModItems.java
+++ src/main/java/net/emmanem/tutorialmod/item/ModItems.java
@@ -4,6 +4,7 @@ import net.emmanem.tutorialmod.TutorialMod;
 import net.emmanem.tutorialmod.item.custom.ChiselItem;
 import net.emmanem.tutorialmod.item.custom.HammerItem;
 import net.emmanem.tutorialmod.item.custom.ModArmorItem;
+import net.emmanem.tutorialmod.sound.ModSounds;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.*;
 import net.minecraft.item.tooltip.TooltipType;
@@ -83,6 +84,9 @@ public class ModItems {
 
     public static final Item KAUPEN_BOW = registerItem("kaupen_bow",
             new BowItem(new Item.Settings().maxDamage(500)));
+
+    public static final Item BAR_BRAWL_MUSIC_DISC = registerItem("bar_brawl_music_disc",
+            new Item(new Item.Settings().jukeboxPlayable(ModSounds.BAR_BRAWL_KEY).maxCount(1)));
 
     // HELPERS
     // Register Items
