f9cbcd3dcf0952775991b90fb0ea22122622cedc
Emma Gaxiola <emma.gaxiol@gmail.com>
2026-08-14 19:39:34 -0600

    added mob variants

--- src/main/java/net/emmanem/tutorialmod/entity/client/MantisRenderer.java
+++ src/main/java/net/emmanem/tutorialmod/entity/client/MantisRenderer.java
@@ -1,14 +1,26 @@
 package net.emmanem.tutorialmod.entity.client;
 
+import com.google.common.collect.Maps;
 import net.emmanem.tutorialmod.TutorialMod;
 import net.emmanem.tutorialmod.entity.custom.MantisEntity;
+import net.emmanem.tutorialmod.entity.custom.MantisVariant;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
+
+import java.util.Map;
 
 public class MantisRenderer extends MobEntityRenderer<MantisEntity, MantisModel<MantisEntity>> {
+    private static final Map<MantisVariant, Identifier> LOCATION_BY_VARIANT =
+            Util.make(Maps.newEnumMap(MantisVariant.class), map -> {
+                map.put(MantisVariant.DEFAULT,
+                        Identifier.of(TutorialMod.MOD_ID, "textures/entity/mantis/mantis.png"));
+                map.put(MantisVariant.ORCHID,
+                        Identifier.of(TutorialMod.MOD_ID, "textures/entity/mantis/mantis_orchid.png"));
+            });
 
     public MantisRenderer(EntityRendererFactory.Context context) {
         super(context, new MantisModel<>(context.getPart(MantisModel.MANTIS)), 0.75f);
@@ -16,7 +28,7 @@ public class MantisRenderer extends MobEntityRenderer<MantisEntity, MantisModel<
 
     @Override
     public Identifier getTexture(MantisEntity entity) {
-        return Identifier.of(TutorialMod.MOD_ID, "textures/entity/mantis/mantis.png");
+        return LOCATION_BY_VARIANT.get(entity.getVariant());
     }
 
     @Override
